554b88642e65dcb1820867dc8449399e18681c43
Eddie_CooRo <1300100eddiecooro@gmail.com>
2019-02-15 01:47:44 +0330

    Fix code-format using prettier

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,49 @@
 import React from 'react';
 
-function Footer(){
-    return(
-        <footer id="footer">
-						<section>
-							<h2>درباره استلر</h2>
-							<p>این دو کارشناس به دنبال محصولات جدید و کارا بودند و می‌خواستند کالاهای مصرفی کاملاً نوینی روانه بازار کنند. برای این کار تکنولوژی مورد نیازشان را خود ابداع کردند. این ایده بعدها یکی از اصولی بود که آنها و همین طور مدیران سال‌های اخیر سونی، از آن پیروی می‌کردند، شرکتی که همواره خواسته همه ...</p>
-							<ul class="actions">
-								<li><a href="generic.html" class="button">کسب اطلاعات بیشتر</a></li>
-							</ul>
-						</section>
-						<section>
-							<h2>ارتباط با ما</h2>
-							<dl class="alt">
-								<dt>آدرس</dt>
-								<dd>بزرگراه اینترنت، خروجی اینترنت شرق، خیابان پهن باند، کوچه سایت شما، پلاک پروتکل امن</dd>
-								<dt>تلفن</dt>
-								<dd>(000) 000-0000 x 0000</dd>
-								<dt>ایمیل</dt>
-								<dd><a href="#">information@untitled.tld</a></dd>
-							</dl>
-							
-						</section>
-						<p class="copyright">© کلیه حقوق برای این سایت محفوظ است. ویرایش و پارسی سازی قالب: <a href="https://mrcode.ir" target="_blank">MrCode.ir</a>.</p>
-					</footer>
-    );
+function Footer() {
+  return (
+    <footer id="footer">
+      <section>
+        <h2>درباره استلر</h2>
+        <p>
+          این دو کارشناس به دنبال محصولات جدید و کارا بودند و می‌خواستند کالاهای
+          مصرفی کاملاً نوینی روانه بازار کنند. برای این کار تکنولوژی مورد
+          نیازشان را خود ابداع کردند. این ایده بعدها یکی از اصولی بود که آنها و
+          همین طور مدیران سال‌های اخیر سونی، از آن پیروی می‌کردند، شرکتی که
+          همواره خواسته همه ...
+        </p>
+        <ul class="actions">
+          <li>
+            <a href="generic.html" class="button">
+              کسب اطلاعات بیشتر
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>ارتباط با ما</h2>
+        <dl class="alt">
+          <dt>آدرس</dt>
+          <dd>
+            بزرگراه اینترنت، خروجی اینترنت شرق، خیابان پهن باند، کوچه سایت شما،
+            پلاک پروتکل امن
+          </dd>
+          <dt>تلفن</dt>
+          <dd>(000) 000-0000 x 0000</dd>
+          <dt>ایمیل</dt>
+          <dd>
+            <a href="#">information@untitled.tld</a>
+          </dd>
+        </dl>
+      </section>
+      <p class="copyright">
+        © کلیه حقوق برای این سایت محفوظ است. ویرایش و پارسی سازی قالب:{' '}
+        <a href="https://mrcode.ir" target="_blank">
+          MrCode.ir
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
-export default Footer;  
+export default Footer;
